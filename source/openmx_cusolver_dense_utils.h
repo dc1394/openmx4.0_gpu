563f69bdbf7ae423cc02a613fc4ed371b71c9ec9
dc1394 <hriver2014-bitbucket@memoad.jp>
2026-05-21 18:51:46 +0900

    Add files that were not included in the commit

---
+++ source/openmx_cusolver_dense_utils.h
@@ -0,0 +1,79 @@
+#ifndef OPENMX_CUSOLVER_DENSE_UTILS_H
+#define OPENMX_CUSOLVER_DENSE_UTILS_H
+
+#include <stdio.h>
+#include <string.h>
+#include "mpi.h"
+
+static void OpenMX_CuSolver_DenseAbort(const char *where, int info)
+{
+  fprintf(stderr,"%s failed, info=%d\n",where,info);
+  fflush(stderr);
+  MPI_Abort(mpi_comm_level1,1);
+}
+
+static void OpenMX_CuSolver_DenseDsyevx_1based(double *A, double *Z, double *W,
+                                               int n, int maxn, const char *where)
+{
+  int l, copy_cols;
+  int info = cusolver_Syevdx(A,W,n,maxn);
+
+  if (info!=0) OpenMX_CuSolver_DenseAbort(where,info);
+
+  for (l=maxn; 1<=l; l--) W[l] = W[l-1];
+
+  if (Z!=NULL && Z!=A){
+    copy_cols = maxn;
+    if (n<copy_cols) copy_cols = n;
+    memcpy(Z,A,sizeof(double)*(size_t)n*(size_t)copy_cols);
+  }
+}
+
+static void OpenMX_CuSolver_DenseDsyevx_0based(double *A, double *Z, double *W,
+                                               int n, int maxn, const char *where)
+{
+  int copy_cols;
+  int info = cusolver_Syevdx(A,W,n,maxn);
+
+  if (info!=0) OpenMX_CuSolver_DenseAbort(where,info);
+
+  if (Z!=NULL && Z!=A){
+    copy_cols = maxn;
+    if (n<copy_cols) copy_cols = n;
+    memcpy(Z,A,sizeof(double)*(size_t)n*(size_t)copy_cols);
+  }
+}
+
+static void OpenMX_CuSolver_DenseZheevx_1based(dcomplex *A, dcomplex *Z, double *W,
+                                               int n, int maxn, const char *where)
+{
+  int l, copy_cols;
+  int info = cusolver_Syevdx_Complex(A,W,n,maxn);
+
+  if (info!=0) OpenMX_CuSolver_DenseAbort(where,info);
+
+  for (l=maxn; 1<=l; l--) W[l] = W[l-1];
+
+  if (Z!=NULL && Z!=A){
+    copy_cols = maxn;
+    if (n<copy_cols) copy_cols = n;
+    memcpy(Z,A,sizeof(dcomplex)*(size_t)n*(size_t)copy_cols);
+  }
+}
+
+static void OpenMX_CuSolver_DenseZheevx_0based(dcomplex *A, dcomplex *Z, double *W,
+                                               int n, int maxn, const char *where)
+{
+  int copy_cols;
+  int info = cusolver_Syevdx_Complex(A,W,n,maxn);
+
+  if (info!=0) OpenMX_CuSolver_DenseAbort(where,info);
+
+  if (Z!=NULL && Z!=A){
+    copy_cols = maxn;
+    if (n<copy_cols) copy_cols = n;
+    memcpy(Z,A,sizeof(dcomplex)*(size_t)n*(size_t)copy_cols);
+  }
+}
+
+#endif
